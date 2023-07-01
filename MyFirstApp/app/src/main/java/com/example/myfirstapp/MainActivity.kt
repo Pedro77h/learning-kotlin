a819a31be7471d3748fc5de691eba5bb2a3340fa
Pedro77h <pedrohenriquearaujosoares25@gmail.com>
2023-07-01 11:36:13 -0300

    my first app kotlin

---
+++ MyFirstApp/app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.myfirstapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myfirstapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding;
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(layoutInflater);
+        setContentView(binding.root);
+
+        binding.buttonOla.setOnClickListener {
+            binding.textView.setText("Ola ${binding.editNome.text.toString()}");
+        }
+
+    }
+}
